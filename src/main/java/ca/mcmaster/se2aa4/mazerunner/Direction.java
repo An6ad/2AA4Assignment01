f20fbbab21594ba09a66daef9ec5e65273ffdde8
An6ad <chhaba21@mcmaster.ca>
2025-02-03 00:59:28 -0500

    implement right hand rule algorithm

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
+++
@@ -1,8 +0,0 @@
-package ca.mcmaster.se2aa4.mazerunner;
-
-//creation of direction class to allow for future implementation of the Liskov Substitution Principle
-public abstract class Direction {
-    public abstract Position move(Position position);
-    public abstract Direction turnRight();
-    public abstract Direction turnLeft();
-}
